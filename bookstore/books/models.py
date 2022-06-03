e8ec4e670c4fcdb9732aa7021aeedb5d7b80180d
Rogalowski <rogalowski@gmail.com>
2022-06-03 17:40:26 +0200

    models

--- bookstore/books/models.py
+++ bookstore/books/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import uuid
+
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Book(models.Model):
+    external_id = models.UUIDField(
+        default=uuid.uuid4,
+        editable=False)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    authors = models.ManyToManyField(Author)
+    published_year = models.IntegerField(default=0)
+    acquired = models.BooleanField()
+    thumbnail = models.TextField(max_length=2048)
+    year = models.SmallIntegerField()

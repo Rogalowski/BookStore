9e0d36c91b7c20518cceee0054e74ec5081a18a5
Rogalowski <rogalowski@gmail.com>
2022-06-24 14:00:38 +0200

    Heroku

--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'books.apps.BooksConfig',
+    # 'books.apps.BooksConfig',
     'rest_framework',
-    # 'books'
+    'books'
 ]
 
 MIDDLEWARE = [
